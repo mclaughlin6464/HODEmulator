4259faa66e70b827616217b59e9ec9f0bbdba50c
Sean McLaughlin <mclaughlin6464@gmail.com>
2016-04-28 22:58:25 -0700

    Sending to Sherlock

---
+++ src/testRedMagic.py
@@ -0,0 +1,7 @@
+#Having trouble with f_c changing the correlation function; I'm not sure if it's a real effect or not. Gonna calculate.
+#TODO stil realtive import problems
+from allCorrFunc import corrFunc
+
+simname = 'fox'
+for f_c in [.2, .4, .6, .8, 1.0]:
+    corrFunc(simname, 1.0, '/u/ki/swmclau2/des/HODOutput/%s/'%simname, f_c = f_c)
