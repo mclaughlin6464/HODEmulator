91f6a7ac9b5025a3eac069809da06c37aa4c6067
Sean McLaughlin <mclaughlin6464@gmail.com>
2016-04-29 15:02:46 -0700

    Added my notebooks.

--- src/testRedMagic.py
+++ src/testRedMagic.py
@@ -2,6 +2,6 @@
 #TODO stil realtive import problems
 from allCorrFunc import corrFunc
 
-simname = 'fox'
+simname = 'chinchilla'
 for f_c in [.2, .4, .6, .8, 1.0]:
-    corrFunc(simname, 1.0, '/u/ki/swmclau2/des/HODOutput/%s/'%simname, f_c = f_c)
+    corrFunc(simname, 1.0, '/u/ki/swmclau2/des/HODOutput/%s/'%simname, f_c = f_c, Lbox = 250.0, npart = 2560)
