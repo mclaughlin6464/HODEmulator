5740a78a8df2d7f48672a4b674d02108e2ee3db9
Sean McLaughlin <mclaughlin6464@gmail.com>
2016-05-05 11:09:20 -0700

    Possibly fixed bug. Moving to test against a chinchilla.

--- src/haloCorrs.py
+++ src/haloCorrs.py
@@ -28,20 +28,20 @@ def _haloCorr(cat, scale_factor, outputdir):
     if outputdir[-1] != '/':
         outputdir+='/'
 
-    #Note: Confusing name between cat and halocat. Consider changing.
+    #Note: Confusing name between cat and ghthalocat. Consider changing.
     halocat = CachedHaloCatalog(simname = cat.simname, halo_finder = cat.halo_finder,version_name = cat.version_name, redshift = redshift)
 
     #Now, calculate with Halotools builtin
     #TODO include the fast version
-    sample_mask = np.logical_and(halocat.halo_table['halo_mvir'] > 7e12, halocat.halo_table['halo_upid'] == -1)
+    sample_mask = halocat.halo_table['halo_mvir'] > 7e12/h#, halocat.halo_table['halo_upid'] == -1)
     x, y, z = [halocat.halo_table[c]*h for c in ['halo_x','halo_y','halo_z'] ] # change units to Mpc/h
     pos = return_xyz_formatted_array(x,y,z, mask = sample_mask)
     #TODO N procs
     print cat
     print halocat.Lbox
-    xi_all = tpcf(pos, RBINS, period = halocat.Lbox, num_threads =  cpu_count())
+    xi_all = tpcf(pos, RBINS, period = halocat.Lbox*h, num_threads =  cpu_count())
 
-    np.savetxt(outputdir + 'xi_all_halo_%.3f_ht_nosh.npy' %(scale_factor), xi_all)
+    np.savetxt(outputdir + 'xi_all_halo_%.3f_ht_ht.npy' %(scale_factor), xi_all)
 
     #xi_all = correlation3d(pos, RBINS, halocat.Lbox)
 
