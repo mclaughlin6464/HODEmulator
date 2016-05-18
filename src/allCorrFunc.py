947a3bf1733247a230bc63871b73c5c33518f96a
mclaughlin6464 <mclaughlin6464@gmail.com>
2016-05-18 14:06:53 -0700

    added in a better way of doing the iterative calcs

--- src/allCorrFunc.py
+++ src/allCorrFunc.py
@@ -9,7 +9,7 @@ from os.path import isdir
 from halotools.empirical_models import HodModelFactory, TrivialPhaseSpace, NFWPhaseSpace
 from halotools.empirical_models import Zheng07Cens, Zheng07Sats
 from halotools.sim_manager import CachedHaloCatalog
-from halotools.mock_observables import return_xyz_formatted_array,tpcf,  tpcf_jackknife, tpcf_one_two_halo_decomp, wp
+from halotools.mock_observables import return_xyz_formatted_array,tpcf, tpcf_jackknife, tpcf_one_two_halo_decomp, wp
 from redMagicHOD import RedMagicCens, RedMagicSats, StepFuncCens, StepFuncSats
 from myCats import *
 
@@ -21,10 +21,10 @@ RBINS = np.logspace(-1, 1.7, 20)
 
 #TODO will need ways to pass params into the model when populating. Could just use kwargs, but how to separate cat kwargs?
 #Could pass in a dict and update the param dict
-def corrFunc(simname, scale_factor, outputdir, plot = False,mMin = 7e12,  **kwargs):
+def corrFunc(simname, scale_factor, outputdir, plot = False,logMmin = 12.1,  **kwargs):
     'Calculate the cross correlation for a single catalog at a single scale factor'
     cat = cat_dict[simname](**kwargs) #TODO better handling of arguements
-    _corrFunc(cat, scale_factor, outputdir, plot, mMin = mMin)
+    _corrFunc(cat, scale_factor, outputdir, plot, mMin = logMmin)
 
 def allCorrFunc(simname, outputdir, plot = False, **kwargs):
     'Calculates cross correlations for all scale factors cached for one halocatalog'
@@ -32,7 +32,7 @@ def allCorrFunc(simname, outputdir, plot = False, **kwargs):
     for a in cat.scale_factors:
         _corrFunc(cat, a, outputdir, plot)
 
-def _corrFunc(cat, scale_factor, outputdir, plot = False, mMin = 7e12):
+def _corrFunc(cat, scale_factor, outputdir, plot = False, logMmin = 12.1):
     'Helper function that uses the built in cat object'
 
     print str(cat)
@@ -51,14 +51,14 @@ def _corrFunc(cat, scale_factor, outputdir, plot = False, mMin = 7e12):
     halocat = CachedHaloCatalog(simname = cat.simname, halo_finder = cat.halo_finder,version_name = cat.version_name, redshift = cat.redshifts[idx])
 
     model = HodModelFactory(
-        #centrals_occupation=RedMagicCens(redshift=cat.redshifts[idx]),
-        centrals_occupation=StepFuncCens(redshift=cat.redshifts[idx], mMin=mMin, h = cat.h),
+        centrals_occupation=RedMagicCens(redshift=cat.redshifts[idx]),
+        #centrals_occupation=StepFuncCens(redshift=cat.redshifts[idx], mMin=mMin, h = cat.h),
         centrals_profile=TrivialPhaseSpace(redshift=cat.redshifts[idx]),
-        #satellites_occupation=RedMagicSats(redshift=cat.redshifts[idx]),
-        satellites_occupation=StepFuncSats(redshift=cat.redshifts[idx]),
+        satellites_occupation=RedMagicSats(redshift=cat.redshifts[idx]),
+        #satellites_occupation=StepFuncSats(redshift=cat.redshifts[idx]),
         satellites_profile=NFWPhaseSpace(redshift=cat.redshifts[idx]))
 
-    #model.param_dict['logMmin'] = 12.5/cat.h
+    model.param_dict['logMmin'] = logMmin/cat.h #this is gonna have to obviously be changed when merged with the other branch.
 
     #Note: slow
     model.populate_mock(halocat, Num_ptcl_requirement = N_PTCL) #TODO try again with 300 or a larger number for more robustness
@@ -85,8 +85,8 @@ def _corrFunc(cat, scale_factor, outputdir, plot = False, mMin = 7e12):
     '''
     #wp_all = wp(pos*cat.h, RBINS, PI_MAX, period=model.mock.Lbox*cat.h, num_threads = cpu_count())
 
-    np.savetxt(outputdir + 'xi_all_%.3f_stepFunc_mm_%.2f.npy' %(scale_factor,mMin), xi_all)
-    #np.savetxt(outputdir + 'xi_cov_%.3f_default.npy' %(scale_factor), xi_cov)
+    np.savetxt(outputdir + 'xi_all_%.3f_default_mm_%.2f.npy' %(scale_factor, logMmin), xi_all)
+    #np.savetxt(outputdir + 'xi_cov_%.3f_default_125_2048.npy' %(scale_factor), xi_cov)
 
     #np.savetxt(outputdir + 'xi_1h_%.3f_stepFunc.npy' %(scale_factor), xi_1h)
     #np.savetxt(outputdir + 'xi_2h_%.3f_stepFunc.npy' %(scale_factor), xi_2h)
