007e30c7500fd18f87940063cfa46ea49223b03b
Sean McLaughlin <mclaughlin6464@gmail.com>
2016-05-19 13:01:18 -0700

    Chasing Emu bugs now.

--- src/testSeveralSteps.py
+++ src/testSeveralSteps.py
@@ -1,7 +1,7 @@
 #I'm going to be testing the correlation functions of the step Function with several different thresholds.
 from allCorrFunc import corrFunc
 
-simname = 'emu'
+simname = 'chinchilla'
 for m in [12.25 + i*0.25 for i in xrange(7)]:
     print 'Log Min Mass: %e'%m
-    corrFunc(simname, 1.0, '/u/ki/swmclau2/des/HODOutput/%s_step_tests/'%simname,logMmin = m)
+    corrFunc(simname, 1.0, '/u/ki/swmclau2/des/HODOutput/%s_step_tests/'%simname,logMmin = m, Lbox = 400.0, npart = 2048)
