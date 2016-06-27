dc7526a4bde22e7d49285c4b75d97ed8315c578a
mclaughlin6464 <mclaughlin6464@gmail.com>
2016-06-27 11:58:05 -0700

    Merged the MCMC branch in, and fixed a few small TODO's

--- src/allCorrFunc.py
+++ src/allCorrFunc.py
@@ -5,11 +5,19 @@
 import numpy as np
 import argparse
 from multiprocessing import cpu_count
-from os.path import isdir
+from os.path import isdir, dirname, abspath, join
 from halotools.empirical_models import HodModelFactory, TrivialPhaseSpace, NFWPhaseSpace
 from halotools.empirical_models import Zheng07Cens, Zheng07Sats
 from halotools.sim_manager import CachedHaloCatalog
 from halotools.mock_observables import return_xyz_formatted_array,tpcf, tpcf_jackknife, tpcf_one_two_halo_decomp, wp
+try:
+    CORRFUNC = True
+    from Corrfunc._countpairs import coutpairs_xi
+
+
+except ImportError:
+    CORRFUNC = False
+
 from redMagicHOD import RedMagicCens, RedMagicSats, StepFuncCens, StepFuncSats
 from myCats import *
 
@@ -17,7 +25,7 @@ N_PTCL = 0
 PI_MAX = 40
 
 RBINS = np.logspace(-1, 1.25, 15)
-#RBIN_CENTERS = (RBINS[1:]+RBINS[:-1])/2 #just for plotting
+RBIN_CENTERS = (RBINS[1:]+RBINS[:-1])/2 #just for plotting
 
 def corrFunc(simname, scale_factor, outputdir,params = {}, n_ptcl = N_PTCL, rbins = RBINS, **kwargs):
     'Calculate the cross correlation for a single catalog at a single scale factor'
@@ -30,8 +38,8 @@ def corrFunc(simname, scale_factor, outputdir,params = {}, n_ptcl = N_PTCL, rbin
     halocat, model = loadHaloAndModel(cat, scale_factor)
     data = popAndCorr(halocat,model, cat, params, n_ptcl, rbins)
 
-    for d, name in zip(data, ['xi_all', 'xi_1h', 'xi_2h', 'wp_all']):
-        np.savetxt(outputdir + '%s_%.3f.npy' %(name, scale_factor), d)
+    np.savetxt(outputdir + 'corr_%.3f_default_mm_%.2f.npy' % (scale_factor, params['logMmin']), data)
+
 
 def allCorrFunc(simname, outputdir,params = {}, n_ptcl = N_PTCL, rbins = RBINS, **kwargs):
     'Calculates cross correlations for all scale factors cached for one halocatalog'
@@ -44,8 +52,8 @@ def allCorrFunc(simname, outputdir,params = {}, n_ptcl = N_PTCL, rbins = RBINS, 
         halocat, model = loadHaloAndModel(cat, a)
         data = popAndCorr(halocat, model, cat, params, n_ptcl, rbins)
 
-        for d, name in zip(data, ['xi_all', 'xi_1h', 'xi_2h', 'wp_all']):
-            np.savetxt(outputdir + '%s_%.3f.npy' % (name, a), d)
+        np.savetxt(outputdir + 'corr_%.3f_default_mm_%.2f.npy' %(a, params['logMmin']), data)
+
 
 def loadHaloAndModel(cat, scale_factor):
     '''Return the cached halo catalog and the appropriate HOD model'''
@@ -62,7 +70,7 @@ def loadHaloAndModel(cat, scale_factor):
         centrals_occupation=RedMagicCens(redshift=cat.redshifts[idx]),
         #centrals_occupation=StepFuncCens(redshift=cat.redshifts[idx]),
         centrals_profile=TrivialPhaseSpace(redshift=cat.redshifts[idx]),
-        satellites_occupation=RedMagicSats(redshift=cat.redshifts[idx],modulate_with_cenocc = True),#TODO consider putting this in redMagicSats intself
+        satellites_occupation=RedMagicSats(redshift=cat.redshifts[idx]),
         #satellites_occupation=StepFuncSats(redshift=cat.redshifts[idx]),
         satellites_profile=NFWPhaseSpace(redshift=cat.redshifts[idx]))
 
@@ -76,13 +84,23 @@ def popAndCorr(halocat,model, cat, params = {}, n_ptcl = N_PTCL, rbins = RBINS):
     model.populate_mock(halocat,Num_ptcl_requirement=n_ptcl)
 
     #Now, calculate with Halotools builtin
-    #TODO include the fast version
     x, y, z = [model.mock.galaxy_table[c] for c in ['x','y','z'] ]
     #mask = model.mock.galaxy_table['halo_mvir'] < 1e15/cat.h
     pos = return_xyz_formatted_array(x,y,z)#, mask = mask)
 
     #TODO N procs
-    xi_all = tpcf(pos*cat.h, RBINS, period = model.mock.Lbox*cat.h, num_threads =  cpu_count())
+    if CORRFUNC:
+        #write bins to file
+        BINDIR = dirname(abspath(__file__))  # location of files with bin edges
+        with open(join(BINDIR , '/binfile'), 'w') as f:
+            for low, high in zip(RBINS[:-1], RBINS[1:]):
+                f.write('\t%f\t%f\n' % (low, high))
+
+        xi_all = coutpairs_xi(model.mock.Lbox*cat.h, cpu_count(), join(BINDIR,'/binfile'), x*cat.h, y*cat.h, z*cat.h )
+        xi_all = np.array(xi_all)
+
+    else:
+        xi_all = tpcf(pos*cat.h, RBINS, period = model.mock.Lbox*cat.h, num_threads =  cpu_count())
 
     #TODO ways to decide which of these to call
     #randoms = np.random.random(pos.shape)*model.mock.Lbox*cat.h
@@ -92,17 +110,17 @@ def popAndCorr(halocat,model, cat, params = {}, n_ptcl = N_PTCL, rbins = RBINS):
 
     xi_1h, xi_2h = tpcf_one_two_halo_decomp(pos*cat.h,
                                             halo_hostid, rbins,
-                                            period=Lbox*cat.h, num_threads=cpu_count(),
+                                            period=model.mock.Lbox*cat.h, num_threads=cpu_count(),
                                             max_sample_size=1e7)
 
     #wp_all = wp(pos*cat.h, RBINS, PI_MAX, period=model.mock.Lbox*cat.h, num_threads = cpu_count())
 
     output = np.stack([RBIN_CENTERS, xi_all, xi_1h, xi_2h])
 
-    np.savetxt(outputdir + 'corr_%.3f_default_mm_%.2f.npy' %(scale_factor, logMmin), output)
-    #np.savetxt(outputdir + 'xi_cov_%.3f_default_125_2048.npy' %(scale_factor), xi_cov)
+    return output
 
-    #np.savetxt(outputdir + 'wp_all_%.3f_default.npy' %(scale_factor), wp_all)
+    #np.savetxt(outputdir + 'corr_%.3f_default_mm_%.2f.npy' %(scale_factor, logMmin), output)
+    #np.savetxt(outputdir + 'xi_cov_%.3f_default_125_2048.npy' %(scale_factor), xi_cov)
 
 if __name__ == '__main__':
     desc = 'Populate a particular halo catalog and calculate cross correlations. '
